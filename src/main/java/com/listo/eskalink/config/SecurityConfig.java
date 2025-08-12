55d6d1c971bed9d78cdc365336de778a113cadcc
ListowelAdolwin <listoweladolwin@gmail.com>
2025-08-12 15:26:31 +0000

    fix: fix swagger cors configuration error

--- src/main/java/com/listo/eskalink/config/SecurityConfig.java
+++ src/main/java/com/listo/eskalink/config/SecurityConfig.java
@@ -69,6 +69,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html", "/api-docs/**").permitAll()
